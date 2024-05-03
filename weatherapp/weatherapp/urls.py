61bb1d4ada70d8393a076ef7f2fffea3444e37f0
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-05-03 23:24:52 +0200

    Add home page

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from weather import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
 ]
