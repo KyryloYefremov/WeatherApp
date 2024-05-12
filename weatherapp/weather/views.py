332d5749352f2818aa656593db33a3a465ea8db0
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-05-12 13:54:03 +0200

    add: getting data from api

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from weather.controllers.weather_api import WeatherApi
+from config import API_KEY
 
 
 def index(request):
-    time = [f"{i}:00" for i in range(24)]
-    return render(request, 'index.html', {'time': time})
+    city = 'Prague'
+    weather_api = WeatherApi(API_KEY)
+    today_forecast = weather_api.get_today_forecast(city)
+    today_forecast['city'] = city
+    return render(request, 'index.html', context={'today_forecast': today_forecast})
