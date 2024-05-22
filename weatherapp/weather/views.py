cb08818fc132b815c2ffd0b68cfcbfaa86326f77
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-05-23 01:41:59 +0200

    add user registration and authentication

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from .forms import SignUpForm, SignInForm
+
 from weather.controllers.weather_api import WeatherApi
+from weather.controllers.user_controller import UserController
 
 import os
 
 DEFAULT_CITY = 'Prague'
 weather_api = WeatherApi(os.getenv('API_KEY'))
+user_controller = UserController('data/users-data.txt')
+user = {
+    'is_authenticated': False,
+    'saved_cities': [],
+}
 
 
 def index(request):
@@ -15,7 +24,7 @@ def get_city_forecast(request, city):
     try:
         today_forecast = weather_api.get_today_forecast(city)
         today_forecast['city'] = city
-        return render(request, 'index.html', context={'today_forecast': today_forecast})
+        return render(request, 'index.html', context={'today_forecast': today_forecast, 'user': user})
     except ValueError as e:
         return render(request, 'index-search-error.html', context={'error': e})
 
@@ -28,8 +37,41 @@ def search_city(request):
 
 
 def sign_in(request):
-    return render(request, 'sign-in.html')
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            result = user_controller.sign_in(form.cleaned_data['email'], form.cleaned_data['password'])
+            if result:
+                user['is_authenticated'] = True
+                return redirect('index')
+
+    form = SignInForm()
+    return render(request, 'sign-in.html', {'form': form})
 
 
+@csrf_exempt
 def sign_up(request):
-    return render(request, 'sign-up.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            result = user_controller.sign_up(
+                form.cleaned_data['email'],
+                form.cleaned_data['password'],
+                form.cleaned_data['card_number'],
+                form.cleaned_data['cvv_code'],
+                form.cleaned_data['expiry_date']
+            )
+            if result:
+                user['is_authenticated'] = True
+                return redirect('index')
+
+    form = SignUpForm()
+    return render(request, 'sign-up.html', {'form': form})
+
+
+def sign_out(request):
+    user['is_authenticated'] = False
+    user['saved_cities'] = []
+    return redirect('index')
