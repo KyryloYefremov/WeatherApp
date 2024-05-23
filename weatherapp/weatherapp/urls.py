af19d9543e97765829c38b2c95024912155553ab
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-05-23 04:22:33 +0200

    add new route param

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -21,7 +21,7 @@ from weather import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:city>', views.get_city_forecast, name='get_city_forecast'),
+    path('<str:city>/<str:forecast_date>/', views.get_city_forecast, name='get_city_forecast'),
     path('search/', views.search_city, name='search_city'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-up/', views.sign_up, name='sign_up'),
