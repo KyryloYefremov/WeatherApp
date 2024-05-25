da00588a8daf43aa595f6f2364f5ba08bc09537a
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-05-26 01:11:22 +0200

    debug=True

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--4rn=j6kiozjlcat0b((t2#-=2*g!04mu)v25jhjdk1wa6h@ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
